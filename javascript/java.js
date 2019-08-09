abd5d3db1b18a46ee99ae33361fc68630e5b4901
TabooThao <daniel.ntaj.thao@gmail.com>
2019-08-09 13:43:35 -0500

    added variables for foundation

--- javascript/java.js
+++ javascript/java.js
@@ -1,3 +1,15 @@
 // api key for this project BfAhixioS9iJQctYVh6YC8qj8xzMIlQt
 
-// adassadas
+// Example of query URL
+// https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey
+
+// ============================================================================================
+// ============================================================================================
+// Variable for Api Key
+const APIKEY = "BfAhixioS9iJQctYVh6YC8qj8xzMIlQt";
+// Variable for User input
+let input = "";
+// link to grab information
+const queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + input + "&api-key=" + APIKEY;
+
+fetch(function)
