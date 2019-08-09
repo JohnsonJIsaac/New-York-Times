f7db70d114306955a36e3c92f9f33da50bb3805c
TabooThao <daniel.ntaj.thao@gmail.com>
2019-08-09 14:33:12 -0500

    Javascript

--- javascript/java.js
+++ javascript/java.js
@@ -7,25 +7,52 @@
 // ============================================================================================
 // Variable for Api Key
 const APIKEY = "BfAhixioS9iJQctYVh6YC8qj8xzMIlQt";
-// Variable for User input
-let term = document.getElementById("searchTerm");
-let records = document.getElementById("numRecords");
-let startyYear = document.getElementById("exampleFormControlInput1");
-let endYear = document.getElementById("exampleFormControlInput2");
-
-// link to grab information
-const queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election" 
-                + term + "&" + records + "&" +startyYear + "&" + endYear + "&api-key=" + APIKEY;
-
-
     // * Take note of various “bugs” that appear with certain searches.
 
+    document.querySelector(".btn.submit.mb-2").addEventListener("click", function(event) {
+        console.log(event);
+        event.preventDefault();
 
-fetch(queryURL).then(function(response) {
-    return response.json();
-    }).then(function(responseJson) {
-    console.log(responseJson);
+        let term = document.getElementById("searchTerm").value;
+        let records = document.getElementById("numRecords").value;
+        let startyYear = document.getElementById("exampleFormControlInput1").value;
+        let endYear = document.getElementById("exampleFormControlInput2").value;
 
-    // document.querySelector("#movie-view").innerHTML = JSON.stringify(responseJson);
-    // console.log(document.querySelector("#movie-view"));
+        // link to grab information
+        const queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election" 
+                        + term + "&" + records + "&" +startyYear + "&" + endYear + "&api-key=" + APIKEY;
+
+        console.log(queryURL);
+            // event.preventDefault();
+
+           
+            fetch(queryURL).then(function(response) {
+                return response.json();
+                }).then(function(responseJson) {
+                console.log(responseJson);
+        
+                // document.querySelector("#movie-view").innerHTML = JSON.stringify(responseJson);
+                // console.log(document.querySelector("#movie-view"));
+                console.log(queryURL);
+
+                })
     })
+
+    document.querySelector(".btn.reset.mb-2").addEventListener("click", function(event) {
+        console.log(event);
+        event.preventDefault();
+            // event.preventDefault();
+            term.innerHTML = "";
+            records.innerHTML = "";
+            startyYear.innerHTML = "";
+            endYear.innerHTML -="";
+    })
+
+    // fetch(queryURL).then(function(response) {
+    //     return response.json();
+    //     }).then(function(responseJson) {
+    //     console.log(responseJson);
+
+    //     // document.querySelector("#movie-view").innerHTML = JSON.stringify(responseJson);
+    //     // console.log(document.querySelector("#movie-view"));
+    //     })
